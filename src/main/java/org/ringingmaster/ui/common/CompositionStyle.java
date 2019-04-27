d2980e0611fd81f6b0bddf24e65ee39e4fe53566
Steve Lake <stevelake@steve-lake-mbp.home>
2019-04-27 08:49:45 +0100

    rename touch to composiiton

--- src/main/java/org/ringingmaster/ui/common/CompositionStyle.java
+++ src/main/java/org/ringingmaster/ui/common/CompositionStyle.java
@@ -11,18 +11,18 @@ import org.slf4j.LoggerFactory;
  * TODO comments???
  * User: Stephen
  */
-public class TouchStyle {
+public class CompositionStyle {
 
     private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-    public enum TouchStyleColor {
+    public enum CompositionStyleColor {
         CALLING_POSITION,
         CALL,
         SPLICE,
         BLOCK,
         VARIANCE,
         GROUP,
-        DEFINITON,
+        DEFINITION,
         PLAIN_LEAD,
         NOTES,
         TITLE,
@@ -35,7 +35,7 @@ public class TouchStyle {
         FALLBACK,
     }
 
-    public enum TouchStyleFont {
+    public enum CompositionStyleFont {
         TITLE,
         AUTHOR,
         MAIN,
@@ -43,83 +43,83 @@ public class TouchStyle {
         NOTES
     }
 
-    private final Color[] colors = new Color[TouchStyleColor.values().length];
-    private final Font[] fonts = new Font[TouchStyleFont.values().length];
+    private final Color[] colors = new Color[CompositionStyleColor.values().length];
+    private final Font[] fonts = new Font[CompositionStyleFont.values().length];
 
-    public TouchStyle() {
-        setColor(TouchStyleColor.CALLING_POSITION, Color.rgb(0, 160, 0));
-        setColor(TouchStyleColor.CALL, Color.rgb(0, 0, 255));
-        setColor(TouchStyleColor.SPLICE, Color.rgb(255, 128, 0));
-        setColor(TouchStyleColor.BLOCK, Color.rgb(140, 140, 140));
-        setColor(TouchStyleColor.VARIANCE, Color.rgb(108, 0, 108));
-        setColor(TouchStyleColor.GROUP, Color.rgb(0, 128, 128));
-        setColor(TouchStyleColor.DEFINITON, Color.rgb(140, 140, 140));
-        setColor(TouchStyleColor.PLAIN_LEAD, Color.rgb(141, 183, 91));
-        setColor(TouchStyleColor.NOTES, Color.rgb(0, 0, 0));
-        setColor(TouchStyleColor.TITLE, Color.rgb(0, 0, 0));
-        setColor(TouchStyleColor.AUTHOR, Color.rgb(0, 0, 129));
-        setColor(TouchStyleColor.UNCHANGED_LH, Color.rgb(170, 170, 170));
-        setColor(TouchStyleColor.CHANGED_LH, Color.rgb(100, 0, 0));
-        setColor(TouchStyleColor.GRID, Color.GREY);
+    public CompositionStyle() {
+        setColor(CompositionStyleColor.CALLING_POSITION, Color.rgb(0, 160, 0));
+        setColor(CompositionStyleColor.CALL, Color.rgb(0, 0, 255));
+        setColor(CompositionStyleColor.SPLICE, Color.rgb(255, 128, 0));
+        setColor(CompositionStyleColor.BLOCK, Color.rgb(140, 140, 140));
+        setColor(CompositionStyleColor.VARIANCE, Color.rgb(108, 0, 108));
+        setColor(CompositionStyleColor.GROUP, Color.rgb(0, 128, 128));
+        setColor(CompositionStyleColor.DEFINITION, Color.rgb(140, 140, 140));
+        setColor(CompositionStyleColor.PLAIN_LEAD, Color.rgb(141, 183, 91));
+        setColor(CompositionStyleColor.NOTES, Color.rgb(0, 0, 0));
+        setColor(CompositionStyleColor.TITLE, Color.rgb(0, 0, 0));
+        setColor(CompositionStyleColor.AUTHOR, Color.rgb(0, 0, 129));
+        setColor(CompositionStyleColor.UNCHANGED_LH, Color.rgb(170, 170, 170));
+        setColor(CompositionStyleColor.CHANGED_LH, Color.rgb(100, 0, 0));
+        setColor(CompositionStyleColor.GRID, Color.GREY);
 
-        setColor(TouchStyleColor.FALLBACK, Color.BLACK);
+        setColor(CompositionStyleColor.FALLBACK, Color.BLACK);
 
-        setFont(TouchStyleFont.TITLE, new Font("Arial", 24));
-        setFont(TouchStyleFont.AUTHOR, new Font("Arial", 27));
-        setFont(TouchStyleFont.MAIN, new Font("Arial", 40));
-        setFont(TouchStyleFont.DEFINITION, new Font("Arial", 30));
-        setFont(TouchStyleFont.NOTES, new Font("Arial", 10));
+        setFont(CompositionStyleFont.TITLE, new Font("Arial", 24));
+        setFont(CompositionStyleFont.AUTHOR, new Font("Arial", 27));
+        setFont(CompositionStyleFont.MAIN, new Font("Arial", 40));
+        setFont(CompositionStyleFont.DEFINITION, new Font("Arial", 30));
+        setFont(CompositionStyleFont.NOTES, new Font("Arial", 10));
     }
 
-    public void setColor(TouchStyleColor type, Color color) {
+    public void setColor(CompositionStyleColor type, Color color) {
         colors[type.ordinal()] = color;
     }
 
-    public Color getColour(TouchStyleColor touchStyleColor) {
-        return colors[touchStyleColor.ordinal()];
+    public Color getColour(CompositionStyleColor compositionStyleColor) {
+        return colors[compositionStyleColor.ordinal()];
     }
 
     public Color getColourFromParseType(ParseType parseType) {
 
         switch (parseType) {
             case CALLING_POSITION:
-                return getColour(TouchStyleColor.CALLING_POSITION);
+                return getColour(CompositionStyleColor.CALLING_POSITION);
             case CALL:
             case CALL_MULTIPLIER:
             case DEFAULT_CALL_MULTIPLIER:
-                return getColour(TouchStyleColor.CALL);
+                return getColour(CompositionStyleColor.CALL);
             case SPLICE:
             case SPLICE_MULTIPLIER:
-                return getColour(TouchStyleColor.SPLICE);
+                return getColour(CompositionStyleColor.SPLICE);
             case PLAIN_LEAD:
             case PLAIN_LEAD_MULTIPLIER:
-                return getColour(TouchStyleColor.PLAIN_LEAD);
+                return getColour(CompositionStyleColor.PLAIN_LEAD);
             case DEFINITION:
             case DEFINITION_MULTIPLIER:
-                return getColour(TouchStyleColor.DEFINITON);
+                return getColour(CompositionStyleColor.DEFINITION);
             case VARIANCE_OPEN:
             case VARIANCE_CLOSE:
-                return getColour(TouchStyleColor.VARIANCE);
+                return getColour(CompositionStyleColor.VARIANCE);
             case MULTIPLIER_GROUP_OPEN:
             case MULTIPLIER_GROUP_OPEN_MULTIPLIER:
             case MULTIPLIER_GROUP_CLOSE:
-                return getColour(TouchStyleColor.GROUP);
+                return getColour(CompositionStyleColor.GROUP);
 //TODO 			case BLOCK_DEFINITION:
 //			case BLOCK_DEFINITION_MULTIPLIER:
-//				return getColour(TouchStyleColor.BLOCK);
+//				return getColour(CompositionStyleColor.BLOCK);
 
             default:
                 log.warn("Getting fallback colour for [{}]", parseType);
-                return getColour(TouchStyleColor.FALLBACK);
+                return getColour(CompositionStyleColor.FALLBACK);
         }
     }
 
-    public void setFont(TouchStyleFont type, Font font) {
+    public void setFont(CompositionStyleFont type, Font font) {
         fonts[type.ordinal()] = font;
     }
 
-    public Font getFont(TouchStyleFont touchStyleFont) {
-        return fonts[touchStyleFont.ordinal()];
+    public Font getFont(CompositionStyleFont compositionStyleFont) {
+        return fonts[compositionStyleFont.ordinal()];
     }
 
 }
